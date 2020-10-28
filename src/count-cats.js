11462c653ddcd94df0cc90e37163744505b4ba1c
Marina Home <golubeva.webmaster@gmail.com>
2020-10-28 20:41:42 +0400

    feat: Count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //console.log('----------------'+"\r\n",matrix);
+  let count = 0;
+  matrix.forEach(arr => {
+    arr.forEach(element => {
+      if (element === '^^')
+        count ++;
+    })
+  });
+  console.log('count: '+count);
+  return count;
 };
