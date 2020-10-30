db298fc88eee1188fcebfdbfae54fdc9ae60b27d
golubeva-webmaster <golubeva.webmaster@gmail.com>
2020-10-30 16:07:50 +0400

    feat: carbon finished

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,21 +4,23 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  console.log('sampleActivity = '+sampleActivity);
   
-  if(typeof sampleActivity.trim() !== 'string')
+  if(typeof sampleActivity !== 'string' || typeof sampleActivity.trim() !== 'string')
     return false;
   else if(
-     Number(sampleActivity) === NaN
-  || Number(sampleActivity) >=15
-  || Number(sampleActivity) <=0
-
+      Number(sampleActivity) === NaN
+      || Number(sampleActivity) >=15
+      || Number(sampleActivity) <=0
   )
     return false;
   else{
     sampleActivity = Number(sampleActivity);
-    let res = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
-    console.log(' res = '+res);
-    return res;
+    if(sampleActivity !== NaN){
+      return false;
+    }
+    else{
+      let res = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+      return res;
+    }
   }
 };
