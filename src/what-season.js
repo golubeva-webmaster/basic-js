b5d7a81fe7392f8254b059486bbe9b1702a5069d
golubeva-webmaster <golubeva.webmaster@gmail.com>
2020-10-30 17:16:10 +0400

    feat: ge Season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined){
+    return "Unable to determine the time of year!";
+  }
+  if (Object.prototype.toString.call(date) != '[object Date]') {
+    throw new Error('THROWN');
+  }
+  let month = date.getMonth();
+  return (month == 11 || month <= 1) ? 'winter':
+          (month <= 4 ? 'spring':
+          (month <= 7 ? 'summer': '/autumn|fall/'))
 };
