e168803b54b356b296b15e817b13432eb660e317
golubeva-webmaster <golubeva.webmaster@gmail.com>
2020-10-30 18:39:27 +0400

    feat: hanoi, array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) === false)
+    throw new Error('THROWN');
+
+  let arrRes = [];
+  for(let i=0; i<=arr.length-1; i++){
+    if(arr[i] === '--discard-next')
+      i = i+2;
+    else if (arr[i] === '--discard-prev')
+      arrRes.pop();
+    else if(arr[i] === '--double-next'){
+      i++;
+      if(arr[i] && typeof arr[i] === 'number')
+        arrRes.push(arr[i],arr[i]);
+    }
+    else if(arr[i] === '--double-prev'){
+      if(arr[i-1])
+        arrRes.push(arr[i-1]);
+    }
+    else
+      arrRes.push(arr[i]);
+
+  }
+  console.log(arrRes);
+  return arrRes;
 };
