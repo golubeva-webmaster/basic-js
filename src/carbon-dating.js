6a646a89329b7bd232585592f19051c3019d471f
golubeva-webmaster <golubeva.webmaster@gmail.com>
2020-10-30 15:47:42 +0400

    feat: carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,22 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  console.log('sampleActivity = '+sampleActivity);
+  
+  if(typeof sampleActivity.trim() !== 'string')
+    return false;
+  else if(
+     Number(sampleActivity) === NaN
+  || Number(sampleActivity) >=15
+  || Number(sampleActivity) <=0
+
+  )
+    return false;
+  else{
+    sampleActivity = Number(sampleActivity);
+    let res = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+    console.log(' res = '+res);
+    return res;
+  }
 };
