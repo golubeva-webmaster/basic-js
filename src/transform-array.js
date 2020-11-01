289e8c438c31b023d7087d8cf0a7254d60cbe3ee
Marina Home <golubeva.webmaster@gmail.com>
2020-11-01 11:33:33 +0400

    feat: transform array is completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,44 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if(arr.includes('somebody'))
-  console.log(arr);
-  if(Array.isArray(arr) === false)
-    throw new Error('THROWN');
-
-  let arrRes = [];
-  for(let i=0; i<=arr.length-1; i++){
-    if(arr[i] != "undefined"){ 
-      if(arr[i] === '--discard-next'){
-        // if(arr[i+2] === '--discard-prev')
-        //   i = i+3;
-        // else if(ar[i+2] === '--double-prev'){
-        //   arrRes.push(arr[i+1]);
-        //   i = i+3;
-        // }
-        // else{}
-
-        // else
-          i = i+2;
-      }
-      else if (arr[i] === '--discard-prev')
-        arrRes.pop();
-      else if(arr[i] === '--double-next'){
-        i++;
-        if(arr[i] && typeof arr[i] === 'number')
-          arrRes.push(arr[i],arr[i]);
-      }
-      else if(arr[i] === '--double-prev'){
-        if(arr[i-1])
-          arrRes.push(arr[i-1]);
-      }
-      else
-        arrRes.push(arr[i]);
+    if (!Array.isArray(arr)) {
+        throw new Error();
     }
-  }
-  ///console.log(arrRes);
-  return arrRes;
+
+    const out = arr.slice();
+    const discard = Symbol("Discarded item");
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                if (i < out.length - 1) {
+                    out[i + 1] = discard;
+                }
+                out[i] = discard;
+                break;
+            case '--discard-prev':
+                if (i > 0) {
+                    out[i - 1] = discard;
+                }
+                out[i] = discard;
+                break;
+            case '--double-next':
+                if (i < out.length - 1) {
+                    out[i] = out[i + 1];
+                } else {
+                    out[i] = discard;
+                }
+                break;
+            case '--double-prev':
+                if (i > 0) {
+                    out[i] = out[i - 1];
+                } else {
+                    out[i] = discard;
+                }
+        }
+    }
+
+    return out.filter(el => el !== discard);
 };
-//Пришлось написать условие, что если i  равен какой-то команде и если i+2 или i -2, тоже команда, то выполнять условия в зависимости от команд. 
-//Причем, у меня были проблемы если i  == "удалить следующее" а i+2  "удалить предыдущее", то я удаляла три элемента начиная с i (включительно). 
-//И еще на входе проверка была на существование элемента, я поправила на if(element != "undefined") , так как false и 0 , не проходили обычную проверку на существовани
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,110 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor (par) {
+        if (par === false) {
+      this.temp = false; // если подается фолс, 
+    } else {
+      this.temp = true;
+    };
+  }
+
+  encrypt(message, key) {
+  //  throw new CustomError('Not implemented');
+    if (message && key) {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+      let keyNew = key;
+      // если длина ключа меньше длины строки
+      if (key.length < message.length) {
+        let k = key.length;
+        for (let i = k; i < message.length; i++) {
+          // чтобы длина ключа совпала с длиной сообщения
+          keyNew = keyNew + keyNew[i - k];
+        };
+      };
+      
+      // строим новую фразу
+      let encrypted = '';
+      let messageLetterPos, 
+        keyLetterPos,
+        encryptedPos;
+      for (let i = 0, j = 0; i < message.length; i++, j++) {
+        if (message[i].match(/[A-Z]/)) { // если лат.буква
+          messageLetterPos = message.charCodeAt(i) - 64;
+          keyLetterPos = keyNew.charCodeAt(j) - 64;
+          encryptedPos = messageLetterPos + (keyLetterPos - 1);  // номер закодированного символа
+          encryptedPos = encryptedPos > 26 ? encryptedPos - 26 : encryptedPos;
+          let encryptedPosCode = encryptedPos + 64; // в кодировке
+          encrypted = encrypted + String.fromCharCode(encryptedPosCode);
+        } else if (message[i].match(/\s/)) { // если пробел, надо сдвинуть ключ
+          encrypted = encrypted + message[i];
+          j = j - 1;
+          } else
+        { // если нет, просто добавляем к строке
+          encrypted = encrypted + message[i];
+        };
+      };
+
+      if (this.temp === false) {
+        return encrypted.split('').reverse().join('');
+      } else {
+        return encrypted;
+      };
+      
+
+
+    } else {
+      throw new Error(); // если не подали сообщение и/или ключ
+    };
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+  //  throw new CustomError('Not implemented');
+  if (message && key) {
+    key = key.toUpperCase(); // а строка уже будет прописными
+      let keyNew = key;
+      // если длина ключа меньше длины строки
+      if (key.length < message.length) {
+        let k = key.length;
+        for (let i = k; i < message.length; i++) {
+          // чтобы длина ключа совпала с длиной сообщения
+          keyNew = keyNew + keyNew[i - k];
+        };
+      };
+      
+      let decrypted = '';
+      let messageLetterPos, 
+        keyLetterPos,
+        decryptedPos;
+
+      for (let i = 0, j = 0; i < message.length; i++, j++) {
+        if (message[i].match(/[A-Z]/)) { // если лат.буква
+          messageLetterPos = message.charCodeAt(i) - 64;
+          keyLetterPos = keyNew.charCodeAt(j) - 64;
+          decryptedPos = messageLetterPos - (keyLetterPos - 1);  // номер закодированного символа
+          decryptedPos = decryptedPos > 26 ? decryptedPos - 26 : decryptedPos;
+          decryptedPos = decryptedPos <= 0 ? 26 + decryptedPos : decryptedPos;
+          let decryptedPosCode = decryptedPos + 64; // в кодировке
+          decrypted = decrypted + String.fromCharCode(decryptedPosCode);
+        } else if (message[i].match(/\s/)) { // если пробел, надо сдвинуть ключ
+          decrypted = decrypted + message[i];
+          j = j - 1;
+          } else
+        { // если нет, просто добавляем к строке
+          decrypted = decrypted + message[i];
+        };
+      };
+
+      if (this.temp === false) {
+        return decrypted.split('').reverse().join('');
+      } else {
+        return decrypted;
+      };
+  } else {
+    throw new Error(); // если не подали сообщение и/или ключ
+  };
+  
   }
 }
 
